6c89093cd0a6a26a535abd5daf4777943474d8b5
emilykbrown <disabledsarcasm@gmail.com>
2025-04-05 21:12:54 -0500

    Navbar and signup form

---
+++ config/variables.php
@@ -0,0 +1,10 @@
+<?php
+
+$emailError = "";
+$passwordError = "";
+$confirmPasswordError = "";
+
+$emailRegex = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+$passwordRegex = '/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{6,}$/';
+
+?>
